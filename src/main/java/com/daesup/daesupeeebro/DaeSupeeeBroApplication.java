ef427bf1183b2020c1c9816a7042b01714bc8e9a
skgur07 <pigjaoki0970@gmail.com>
2024-11-30 17:25:51 +0900

    init

---
+++ src/main/java/com/daesup/daesupeeebro/DaeSupeeeBroApplication.java
@@ -0,0 +1,13 @@
+package com.daesup.daesupeeebro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeSupeeeBroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaeSupeeeBroApplication.class, args);
+    }
+
+}
